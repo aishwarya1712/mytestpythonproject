184712ac6a366a6787d3860b704ecf9b83c1800f
Aishwarya Sriram <98.aishu@gmail.com>
2023-11-02 18:18:14 -0700

    used sequential chain instead of simplesequential chain

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ from langchain.llms import OpenAI
 
 from langchain.prompts import PromptTemplate 
 from langchain.chains import LLMChain
-from langchain.chains import SimpleSequentialChain
+# from langchain.chains import SimpleSequentialChain
+from langchain.chains import SequentialChain
 
 os.environ['OPENAI_API_KEY'] = apikey # dictionary
 
@@ -32,20 +33,38 @@ script_template = PromptTemplate(
 # LLMS
 llm = OpenAI(temperature=0.9)
 
+# ----- Simple Sequential Chain --------
 # to use the above templates, we need to have an LLM chain
-title_chain = LLMChain(llm=llm, prompt=title_template,verbose=True)
-script_chain = LLMChain(llm=llm, prompt=script_template, verbose=True)
+# title_chain = LLMChain(llm=llm, prompt=title_template,verbose=True)
+# script_chain = LLMChain(llm=llm, prompt=script_template, verbose=True)
 
 # connect the chains together
-seq_chain = SimpleSequentialChain(chains=[title_chain, script_chain], verbose=True) # order is important here!! 
 # the output of the first goes into the input of the second - simple sequential chain
-
-# trigger prompt to LLM
-if(prompt):
-    response = seq_chain.run(prompt) # gets response from LLM
-    st.write(response) # displays on screen
-
+# seq_chain = SimpleSequentialChain(chains=[title_chain, script_chain], verbose=True) # order is important here!! 
 
 # the simple sequential chain only displays the output of the LAST output of the llm chain.
 # to grab multiple outputs, it gets trickier.
 # we can use SequentialChain instead of SSQ to get multiple outputs.
+#seq_chain = SimpleSequentialChain(chains=[title_chain, script_chain], verbose=True) # order is important here!! 
+
+# trigger prompt to LLM - For SSQ
+# if(prompt):
+    # response = seq_chain.run(prompt) # gets response from LLM for SSQ
+    # st.write(response) # displays on screen
+
+# ----- Sequential Chain --------
+# to use the above templates, we need to have an LLM chain
+title_chain = LLMChain(llm=llm, prompt=title_template,verbose=True, output_key='title')
+script_chain = LLMChain(llm=llm, prompt=script_template, verbose=True, output_key='script')
+
+# connect the chains together
+# the output of the first goes into the input of the second - sequential chain
+seq_chain = SequentialChain(chains=[title_chain, script_chain], input_variables=['topic'], output_variables=['title','script'], verbose=True) # order is important here!! 
+
+# trigger prompt to LLM
+if(prompt):
+    response = seq_chain({'topic':prompt})
+    st.write(response['title']) # displays on screen
+    st.write(response['script']) # displays on screen
+
+
